ce338555220290021c731f0863dac45951ead3c1
palestamp <staskozhevets@gmail.com>
2019-01-05 03:55:25 +0300

    move decoding to utils

---
+++ pkg/machinery/decode/struct.go
@@ -0,0 +1,31 @@
+package decode
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/mitchellh/mapstructure"
+	"github.com/pkg/errors"
+)
+
+func Decode(from interface{}, into interface{}) error {
+	var m mapstructure.Metadata
+	decoder, err := mapstructure.NewDecoder(&mapstructure.DecoderConfig{
+		Result:   into,
+		Metadata: &m,
+	})
+	if err != nil {
+		return errors.WithMessage(err, "resource decoding failed")
+	}
+
+	if err := decoder.Decode(from); err != nil {
+		return errors.WithMessage(err, "resource decoding failed")
+	}
+
+	if len(m.Unused) != 0 {
+		err = fmt.Errorf("unknown fields: %s", strings.Join(m.Unused, ", "))
+		return errors.WithMessage(err, "resource decoding failed")
+	}
+
+	return nil
+}
